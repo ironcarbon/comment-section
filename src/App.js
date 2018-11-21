cf2981c54009ec966d138f20ea5dcd5ad82657d4
ipek <ipekcivicakan@gmail.com>
2018-11-20 21:31:03 -0800

    Implementing an approval card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 import faker from 'faker';
 
@@ -8,9 +9,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CommentDetail author="Sam" timeAgo="Today 6:00PM" comment="Great pic!" avatar={faker.image.avatar()}/>
-        <CommentDetail author="Alex" timeAgo="Today 5:00AM" comment="Nice blog post" avatar={faker.image.avatar()}/>
-        <CommentDetail author="Bill" timeAgo="Yesterday 3:00PM" comment="Hello" avatar={faker.image.avatar()}/>
+        <ApprovalCard>
+          <div>
+            <h4>Warning!</h4>
+            Are you sure you want to do this?
+          </div>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Sam" timeAgo="Today 6:00PM" comment="Great pic!" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Alex" timeAgo="Today 5:00AM" comment="Nice blog post" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Bill" timeAgo="Yesterday 3:00PM" comment="Hello" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+          
+        
+       
       </div>
     );
   }
